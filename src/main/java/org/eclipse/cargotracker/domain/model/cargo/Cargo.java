5d9d481c19e9469a5d4a184d70fbc61c9a408d25
hantsy <hantsy@gmail.com>
2026-06-04 11:53:12 +0800

    chore: clean up

--- src/main/java/org/eclipse/cargotracker/domain/model/cargo/Cargo.java
+++ src/main/java/org/eclipse/cargotracker/domain/model/cargo/Cargo.java
@@ -103,11 +103,10 @@ public class Cargo implements Serializable {
         // Cargo origin never changes, even if the route specification changes.
         // However, at creation, cargo origin can be derived from the initial
         // route specification.
-        this.origin = routeSpecification.getOrigin();
-        this.routeSpecification = routeSpecification;
+        this.origin = routeSpecification.origin();
 
-        this.delivery = Delivery.derivedFrom(this.routeSpecification, this.itinerary, HandlingHistory.EMPTY);
-        this.itinerary = Itinerary.EMPTY_ITINERARY;
+        this.delivery = DeliveryFactory.create(this.routeSpecification, this.itinerary, HandlingHistory.EMPTY);
+        this.itinerary = Itinerary.EMPTY;
     }
 
     public TrackingId getTrackingId() {
@@ -148,7 +147,7 @@ public class Cargo implements Serializable {
 
         this.routeSpecification = routeSpecification;
         // Handling consistency within the Cargo aggregate synchronously
-        this.delivery = delivery.updateOnRouting(this.routeSpecification, this.itinerary);
+        this.delivery = DeliveryFactory.updateOnRouting(this.delivery, this.routeSpecification, this.itinerary);
     }
 
     public void assignToRoute(Itinerary itinerary) {
@@ -157,7 +156,7 @@ public class Cargo implements Serializable {
         this.itinerary = itinerary;
 
         // Handling consistency within the Cargo aggregate synchronously
-        this.delivery = delivery.updateOnRouting(this.routeSpecification, this.itinerary);
+        this.delivery = DeliveryFactory.updateOnRouting(this.delivery, this.routeSpecification, this.itinerary);
     }
 
     /**
@@ -175,7 +174,7 @@ public class Cargo implements Serializable {
      * @param handlingHistory handling history
      */
     public void deriveDeliveryProgress(HandlingHistory handlingHistory) {
-        this.delivery = Delivery.derivedFrom(getRouteSpecification(), getItinerary(), handlingHistory);
+        this.delivery = DeliveryFactory.create(getRouteSpecification(), getItinerary(), handlingHistory);
         // LOGGER.log(Level.INFO, "deriveDeliveryProgress: {0}", this.delivery);
     }
 
